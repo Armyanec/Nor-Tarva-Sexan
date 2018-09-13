9e60d7a32708c9138a0f401380208253c52d08f2
Armyanec <aram.v.hovakyan@gmail.com>
2018-09-14 01:37:40 +0400

    Repackaging.

---
+++ src/com/company/receptner/Tolma.java
@@ -0,0 +1,65 @@
+package com.company.receptner;
+
+public class Tolma {
+
+    private String brindz;
+    private String karag;
+    private String lobi;
+    private String dzavar;
+    private String kaxamb;
+    private String plita;
+    private String aman;
+    private String sox;
+
+
+    String tolmifarsh  (){
+        return this.brindz + this.lobi + this.sox ;
+    }
+
+    String patateluproces (){
+        return this.kaxamb;
+    }
+
+    String tolmaepel (){
+        return this.plita + this.aman;
+    }
+
+    @Override
+    public String toString() {
+        return "Tolma{" +
+                "brindz='" + brindz + '\'' +
+                ", karag='" + karag + '\'' +
+                ", lobi='" + lobi + '\'' +
+                ", dzavar='" + dzavar + '\'' +
+                ", kaxamb='" + kaxamb + '\'' +
+                ", plita='" + plita + '\'' +
+                ", aman='" + aman + '\'' +
+                ", sox='" + sox + '\'' +
+                '}';
+    }
+//_______________________________________________________
+         public String getBrindz (){
+         return this.brindz;
+
+        }
+        public  void setBrindz(String brindz){
+        this.brindz = brindz;
+        }
+//_________________________________________________________
+
+    public  String getKarag() {
+        return this.karag;
+    }
+
+        public void setKarag(String karag){
+
+
+
+            if (karag.equals ("karag")){
+
+                this.karag = "15 kilo";
+            }
+
+        }
+    }
+
---
+++ src/com/company/receptner/Tort.java
@@ -0,0 +1,118 @@
+package com.company.receptner;
+
+public class Tort {
+
+
+    // region Instance Fields / Local popoxakanner
+    private String dzu;
+    private String alyur ;
+    private String dzet;
+    private String shaqaravaz ;
+    private String duxovka;
+    private String mom;
+    private String alur;
+    private String electrakanutyun;
+
+    // endregion
+
+
+    // region Local Methods / Local metodner
+    String patrastelHumtort() {
+
+        return this.dzet + this.dzu + this.alur;
+
+    }
+
+
+    String epel() {
+
+        return this.duxovka + this.electrakanutyun;
+    }
+
+
+    String krem() {
+
+        return this.dzu + this.alyur + this.shaqaravaz;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Tort{" +
+                "dzu='" + dzu + '\'' +
+                ", alyur='" + alyur + '\'' +
+                ", dzet='" + dzet + '\'' +
+                ", shaqaravaz='" + shaqaravaz + '\'' +
+                ", duxovka='" + duxovka + '\'' +
+                ", mom='" + mom + '\'' +
+                ", alur='" + alur + '\'' +
+                ", electrakanutyun='" + electrakanutyun + '\'' +
+                '}';
+    }
+
+    // endregion
+
+    // region GETTER / SETTER
+    public String getDzu(){
+        return this.dzu;
+    }
+
+    public void setDzu(String dzu){
+        if (!dzu.equals("pchacac dzu")) {
+            this.dzu = dzu;
+        }
+    }
+//__________________________________
+
+    public  String getAlyur(){
+        return  this.alur;
+    }
+    public void setAlyur(String alur){
+        if (!alur.equals("sev guyn")){
+            this.alur = alur;
+        }
+    else this.alur = "kakachka";
+    }
+
+    //____________________________
+
+    public  String getDzet() {
+        return this.dzet;
+}
+
+
+    public void setDzet(String dzet){
+    if (dzet.equals("pakas"))
+        this.dzet = "2 litr";
+
+    }
+//____________________________________________
+
+    public  String getShaqaravaz(){
+        return this.shaqaravaz;
+    }
+public void setShaqaravaz(String shaqaravaz){
+        this.shaqaravaz = shaqaravaz;
+}
+//____________________________________________
+
+    public String getDuxovka(){
+        return this.duxovka;
+    }
+
+public  void setDuxovka(String duxovka){
+        if (duxovka.equals("tortlar"))
+
+        this.duxovka = duxovka;
+}
+//______________________________________________
+
+
+
+
+    // endregion
+
+
+
+}
