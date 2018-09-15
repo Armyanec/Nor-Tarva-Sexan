a796619952c6fcc78db964d2f8d80d12983bd4f5
Armyanec <aram.v.hovakyan@gmail.com>
2018-09-16 00:27:23 +0400

    Repackaging.

--- src/com/company/receptner/Masivner15.java
+++ src/com/company/receptner/Masivner15.java
@@ -1,5 +1,7 @@
 package com.company.receptner;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Masivner15 {
@@ -29,23 +31,57 @@ public class Masivner15 {
 //        если стаж от 5 до 10 лет - 5%. Ввести с клавиатуры зарплату и стаж, вывести надбавку
 //        и сумму к выплате.
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("zarplata");
-        double zarplata = in.nextInt();
-        System.out.println("staj");
-        int staj = in.nextInt();
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("zarplata");
+//        double zarplata = in.nextInt();
+//        System.out.println("staj");
+//        int staj = in.nextInt();
+//
+//        if (staj > 2 && staj < 5){
+//
+//            System.out.println(zarplata+((zarplata*2)/100) );
+//        }
+//else if (staj>=5 && staj < 10){
+//
+//            System.out.println( zarplata+((zarplata*5)/100) );
+//        }
 
-        if (staj > 2 && staj < 5){
+//_____________________________________________________
+//    Ввести с клавиатуры значения трех сторон треугольника a, b и c и
+//    определить, является ли он прямоугольным. Ответ вывести в виде сообщения.
+        List<Float> masiv = new ArrayList <Float>();
 
-            System.out.println(zarplata+((zarplata*2)/100) );
+        Scanner f = new Scanner(System.in);
+
+        float x = f.nextInt();
+        masiv.add(x);
+        float y = f.nextInt();
+        masiv.add(y);
+        float z = f.nextInt();
+        masiv.add(z);
+
+
+        for (int i = 0; i < masiv.size(); i++) {
+
+
+            for (int j = 0; j < masiv.size(); j++) {
+
+
+                if (masiv.get(i) > masiv.get(j)){
+                    masiv.get(i) = masiv.get(j);
+                }
+
+
+            }
         }
-else if (staj>=5 && staj < 10){
-
-            System.out.println( zarplata+((zarplata*5)/100) );
-        }
 
 
+                     if (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) {
 
-    }
+                         System.out.println("uxankyun erankyun");
 
-}
+                     } else System.out.println("uxankyun erankyun chi");
+
+                 }
+             }
+
